340d863e45fbe278696604201644ceac554b5236
Vansh Kushwaha <thecodesmithofficial@gmail.com>
2024-06-15 00:58:57 +0530

    adding files

--- app/src/main/java/com/tcs/games/scorefour/ui/login/LoginFragment.kt
+++ app/src/main/java/com/tcs/games/scorefour/ui/login/LoginFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.tcs.games.scorefour.databinding.FragmentGalleryBinding
 
-class GalleryFragment : Fragment() {
+class LoginFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
 
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val loginViewModel =
+            ViewModelProvider(this).get(LoginViewModel::class.java)
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        loginViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
